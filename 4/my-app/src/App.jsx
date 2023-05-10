f98ae2e510fcdc614a36cb5d07d67245305a76da
sway <40759948+sway9119@users.noreply.github.com>
2023-05-10 09:15:55 +0900

    style属性とJavaScriptの実装

--- 4/my-app/src/App.jsx
+++ 4/my-app/src/App.jsx
@@ -1,8 +1,21 @@
+// コンポーネント
 export const App = () => {
+  const onClickButton = () => {
+    alert("アラートです");
+  };
+
+  // CSSオブジェクト
+  // CSSのスタイル名はキャメルケースになる
+  const contentStyle = {
+    color: "blue",
+    fontSize: "20px",
+  };
+
   return (
     <>
       <h1>hello world</h1>
-      <p>お元気ですか？</p>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
