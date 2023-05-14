9508ffeae544e86d7e60f569b76e2a49e67aaf14
sway <40759948+sway9119@users.noreply.github.com>
2023-05-14 09:24:02 +0900

    StyleJsxのテスト

--- 5/my-app/src/App.js
+++ 5/my-app/src/App.js
@@ -1,9 +1,11 @@
 import { CssModules } from "./modules/CssModules";
+import { StyleJsx } from "./modules/StyledJsx";
 
 export const App = () => {
   return (
     <>
       <CssModules></CssModules>
+      <StyleJsx></StyleJsx>
     </>
   );
 };
