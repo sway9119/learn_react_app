8c33fb25e44e37b057330ae807640a5c914d8b90
sway <40759948+sway9119@users.noreply.github.com>
2023-05-17 08:49:47 +0900

    styled componentsのテスト

--- 5/my-app/src/App.js
+++ 5/my-app/src/App.js
@@ -1,11 +1,13 @@
 import { CssModules } from "./modules/CssModules";
 import { StyleJsx } from "./modules/StyledJsx";
+import { StyledComponents } from "./modules/StyledComponents";
 
 export const App = () => {
   return (
     <>
       <CssModules></CssModules>
       <StyleJsx></StyleJsx>
+      <StyledComponents></StyledComponents>
     </>
   );
 };
