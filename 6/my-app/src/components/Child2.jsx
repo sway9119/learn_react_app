76a6f748f5b1e4917ad8fa813a44fcc04a842913
sway <40759948+sway9119@users.noreply.github.com>
2023-05-21 11:29:39 +0900

    再レンダリングのテスト

---
+++ 6/my-app/src/components/Child2.jsx
@@ -0,0 +1,13 @@
+const style = {
+  height: "50px",
+  backgroundColor: "lightgray",
+};
+
+export const Child2 = () => {
+  console.log("Child2レンダリング");
+  return (
+    <div style={style}>
+      <p>Child2</p>
+    </div>
+  );
+};
