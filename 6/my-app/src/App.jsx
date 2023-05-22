2db6106c0ee09c8a0ee1eaabee37d54ebb57be44
sway <40759948+sway9119@users.noreply.github.com>
2023-05-22 18:51:27 +0900

    関数の再生成のテスト

--- 6/my-app/src/App.jsx
+++ 6/my-app/src/App.jsx
@@ -1,22 +1,27 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, memo, useCallback } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
-export const App = () => {
+export const App = memo(() => {
   console.log("Appレンダリング");
 
   const [num, setNum] = useState(0);
+
   const onClickButton = () => {
     setNum(num + 1);
   };
+
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
 
   return (
     <>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
-      <Child1></Child1>
+      <Child1 onClickReset={onClickReset}></Child1>
       <Child4></Child4>
     </>
   );
-};
+});
